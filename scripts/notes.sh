390fe0f8b05985ebdee37171420966f8c6d389ec
Eagen <sanshengshiwx@gmail.com>
2024-03-26 16:26:09 +0800

    使用wxd的OpenAI API密钥，使用了第三方服务端；在Notes output中增加readme.txt

--- scripts/notes.sh
+++ scripts/notes.sh
@@ -1,4 +1,4 @@
-droidbot -a apks/com.simplemobiletools.notes.pro_105.apk \
+python3 start.py -a ../Data_AutoDroid/apks/com.simplemobiletools.notes.pro_105.apk \
     -o output/notes -is_emulator \
     -task "Create a checklist note called 'NewCheckList'" \
     -keep_app -keep_env
